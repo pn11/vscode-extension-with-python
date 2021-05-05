ecae67fab83f290939fb2f70a0f2be212bfc9c9a
oka <pn11@users.noreply.github.com>
2021-05-06 01:58:46 +0900

    first draft

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Options, PythonShell } from 'python-shell';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,8 +17,56 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('vscode-extension-with-python.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-extension-with-python!');
+		// List all extensions (yours comes last)
+		// vscode.extensions.all.filter( e => {console.log(e)});
+
+		// get path to extension
+		console.log(vscode.extensions.getExtension('undefined_publisher.vscode-extension-with-python')?.extensionPath);
+		let ext_path = vscode.extensions.getExtension('undefined_publisher.vscode-extension-with-python')?.extensionPath;
+
+		// get pythonpath from VSCode config
+		let pythonpath = vscode.workspace.getConfiguration('python').get<string>('pythonPath');
+		console.log(pythonpath);
+
+		let options1: Options = {
+			mode: 'text', // response in text format
+			pythonPath: pythonpath,
+			pythonOptions: ['-u'], // get print results in real-time
+		};
+		PythonShell.runString(
+			'\n\
+import sys\n\
+print(sys.executable)\n\
+print(sys.version)\n\
+import os\n\
+print(os.getcwd())', options1, function (err, res) {
+			console.log('Test executing a python string');
+			if (err) {
+				console.log(err);
+				throw err;
+			}
+			res?.filter(r => {console.log(r)});
+		});
+
+		let options2: Options = {
+			mode: 'text',
+			pythonPath: pythonpath,
+			pythonOptions: ['-u'], // get print results in real-time
+			scriptPath: ext_path,
+			args: ['value1', 'value2', 'value3']
+		};
+
+		PythonShell.run('python/hello.py', options2, function (err, res) {
+			console.log('Test executing a python script');
+			if (err) {
+				console.log(err);
+				throw err;
+			}
+			const res_str: string = (res || [''])[0];
+			console.log(res_str);
+			// Display a message box to the user
+			vscode.window.showInformationMessage(res_str);
+		});
 	});
 
 	context.subscriptions.push(disposable);
